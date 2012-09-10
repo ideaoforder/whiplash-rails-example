ec6e15a67487faf514ee39f5ece51a0c4042522b
Mark Dickson <mark@sitesteaders.com>
2012-09-10 15:55:55 -0400

    creds

--- app/models/whiplash.rb
+++ app/models/whiplash.rb
@@ -1,6 +1,6 @@
 class Whiplash < ActiveResource::Base
   class_attribute :settings
-  self.settings = {'X-API-KEY' => 'PAfPapjZKYfYeZ2B97qf' }
+  self.settings = {'X-API-KEY' => 'XXXXXXXXXXX' }
   self.site = 'http://localhost:3000/api/'
   self.format = :json
 end
