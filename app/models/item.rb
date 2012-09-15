135aca09f149ddc510bd458850b9520c3a468ca9
Mark Dickson <mark@sitesteaders.com>
2012-09-14 20:35:26 -0400

    cleanup, new index page, start of doc

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < Whiplash
   # Unfortunately, headers don't inherit from the parent, so they have to be set like so:
   headers['X-API-KEY'] = Whiplash.settings['X-API-KEY']
-  
-  # attr_accessible :available, :created_at, :description, :exp_quantity, :height, :id, :image_file_name, :image_originator_url, :image_updated_at, :length, :media_mail, :originator, :originator_id, :packaging, :price, :quantity, :sku, :title, :updated_at, :vendor, :weight, :width
+
 end
--- app/models/whiplash.rb
+++ app/models/whiplash.rb
@@ -1,6 +1,6 @@
 class Whiplash < ActiveResource::Base
   class_attribute :settings
-  self.settings = {'X-API-KEY' => 'XXXXXXXXXXX' }
+  self.settings = {'X-API-KEY' => 'XXXXXXXXX' }
   self.site = 'http://localhost:3000/api/'
   self.format = :json
 end
