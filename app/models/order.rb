50b0f04901f676f92e1e2265ef7fbed388f67a46
James Marks <marks@vgkids.com>
2012-09-14 23:09:12 -0700

    tweaks to readme, sandbox key saved to project

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < Whiplash
   # Unfortunately, headers don't inherit from the parent, so they have to be set like so:
-  headers['X-API-KEY'] = Whiplash.settings['X-API-KEY']
+  headers['X-API-KEY'] = Whiplash.settings['X-API-KEY']  
 
 end
--- app/models/whiplash.rb
+++ app/models/whiplash.rb
@@ -1,7 +1,7 @@
 class Whiplash < ActiveResource::Base
   class_attribute :settings
-  self.settings = {'X-API-KEY' => 'XXXXXXXXX' }
-  self.site = 'http://localhost:3000/api/'
+  self.settings = {'X-API-KEY' => 'Hc2BHTn3bcrwyPooyYTP' } # Shared Sandbox key for the testing environment
+  self.site = 'http://testing.whiplashmerch.com/api/'
   self.format = :json
 end
   
